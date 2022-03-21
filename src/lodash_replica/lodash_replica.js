e6efc1f57919d44f0076f49479931c340e907a62
BrianJenney <brianjenney83@gmail.com>
2022-03-21 06:47:31 -0700

    add starter code for lodash stuff

--- src/lodash_replica/lodash_replica.js
+++ src/lodash_replica/lodash_replica.js
@@ -1,12 +1,17 @@
-const myFlatMap = undefined;
+const myFlatMap = (arr) => {
+    // your code here
+};
 
-const myConcat = undefined;
+const myConcat = (arr, values) => {
+    // your code here
+};
 
-const myReject = undefined;
-
+const myReject = (arr, itemToReject) => {
+    // your code here
+};
 
 module.exports = {
     myFlatMap,
     myConcat,
-    myReject
-}
+    myReject,
+};
