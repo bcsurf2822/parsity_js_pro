ad2339ed7705b2b102b55cf46edb3d59e9893d3f
Brian Jenney <brianj@Brian-Jenney-MBP16-2019.local>
2022-11-10 10:11:34 -0800

    fix lodash tests

--- src/lodash_replica/lodash_replica.test.js
+++ src/lodash_replica/lodash_replica.test.js
@@ -1,27 +1,21 @@
-const { myFlatMap, myConcat, myReject } = require('./lodash_replica');
+const { myFlatMap, myConcat, myReject } = require("./lodash_replica");
 
-describe.skip('myFlatMap', () => {
-    it('flattens an array', () => {
-        expect(myFlatMap([1, 2, [3, 4, 5, 6, [7, 8, 9, [10]]]])).toEqual([
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        ]);
-    });
+describe.skip("myFlatMap", () => {
+  it("flattens an array", () => {
+    expect(myFlatMap([1, 2, [3, 4, 5, 6, [7, 8, 9, [10]]]])).toEqual([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ]);
+  });
 });
 
-describe.skip('myConcat', () => {
-    it('concatenates multiple items in an array', () => {
-        expect(myConcat([1], [2], [3]).toEqual([1, 2, 3]));
-    });
+describe.skip("myConcat", () => {
+  it("concatenates multiple items in an array", () => {
+    expect(myConcat([1], [2], [3])).toEqual([1, 2, 3]);
+  });
 });
 
-describe.skip('myReject', () => {
-    it('rejects an item in an array', () => {
-        expect(myReject([1, 2, 3], 3).toEqual([1, 2]));
-        expect(
-            myReject(['harry', 'john', 'sally'], 'john').toEqual([
-                'harry',
-                'sally',
-            ])
-        );
-    });
+describe.skip("myReject", () => {
+  it("rejects an item in an array", () => {
+    expect(myReject([1, 2, 3], 3)).toEqual([1, 2]);
+  });
 });
