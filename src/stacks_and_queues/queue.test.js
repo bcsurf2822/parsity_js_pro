4ddab45a5d1d2c383301e410ed4d15cfb5c65bef
Paul Mun <pmun831@gmail.com>
2022-12-30 15:30:18 -0500

    added skip method to tests

--- src/stacks_and_queues/queue.test.js
+++ src/stacks_and_queues/queue.test.js
@@ -1,6 +1,6 @@
 const { Queue } = require("./queue");
 
-describe("Queue", () => {
+describe.skip("Queue", () => {
   it("should enqueue an item", () => {
     const queue = new Queue();
     queue.enqueue(1);
--- src/stacks_and_queues/stacks.test.js
+++ src/stacks_and_queues/stacks.test.js
@@ -1,6 +1,6 @@
 const { Stack } = require("./stacks");
 
-describe("Stack", () => {
+describe.skip("Stack", () => {
   it("should push an item", () => {
     const stack = new Stack();
     stack.push(1);
@@ -16,7 +16,7 @@ describe("Stack", () => {
     expect(stack.pop()).toBe(2);
     expect(stack.items).toEqual([1]);
   });
-  it.skip("should peek an item", () => {
+  it("should peek an item", () => {
     const stack = new Stack();
     stack.push(1);
     stack.push(2);
@@ -24,12 +24,12 @@ describe("Stack", () => {
     expect(stack.peek()).toBe(2);
     expect(stack.items).toEqual([1, 2]);
   });
-  it.skip("should return true if the stack is empty", () => {
+  it("should return true if the stack is empty", () => {
     const stack = new Stack();
 
     expect(stack.isEmpty()).toBe(true);
   });
-  it.skip("should return false if the stack is not empty", () => {
+  it("should return false if the stack is not empty", () => {
     const stack = new Stack();
     stack.push(1);
     stack.push(2);
@@ -37,7 +37,7 @@ describe("Stack", () => {
     expect(stack.isEmpty()).toBe(false);
   });
 
-  it.skip("should clear the stack", () => {
+  it("should clear the stack", () => {
     const stack = new Stack();
     stack.push(1);
     stack.push(2);
@@ -45,7 +45,7 @@ describe("Stack", () => {
 
     expect(stack.items).toEqual([]);
   });
-  it.skip("should print the stack", () => {
+  it("should print the stack", () => {
     const stack = new Stack();
     stack.push(1);
     stack.push(2);
