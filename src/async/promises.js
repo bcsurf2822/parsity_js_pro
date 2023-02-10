e4da97999f53edb8f53fcc26a4ec986cab315259
Brian Jenney <brianj@Brian-Jenney-MBP16-2019.local>
2023-02-10 11:58:25 -0800

    add promise all addition

--- src/async/promises.js
+++ src/async/promises.js
@@ -1,37 +1,45 @@
 const fakeApiCall = (args) => {
-    return new Promise((resolve) => {
-        resolve({ data: args });
-    });
+  return new Promise((resolve) => {
+    resolve({ data: args });
+  });
 };
 
 const getDataPromiseChain = () => {
-    const retVal = {};
-    return fakeApiCall({ name: 'Heather' }).then((res) => {
-        retVal.name = res.data.name;
+  const retVal = {};
+  return fakeApiCall({ name: "Heather" }).then((res) => {
+    retVal.name = res.data.name;
 
-        fakeApiCall({ job: 'code monkey' }).then((res) => {
-            retVal.job = res.data.job;
+    fakeApiCall({ job: "code monkey" }).then((res) => {
+      retVal.job = res.data.job;
 
-            fakeApiCall({ age: 420 }).then((res) => {
-                retVal.age = res.data.age;
-                return retVal;
-            });
-        });
+      fakeApiCall({ age: 420 }).then((res) => {
+        retVal.age = res.data.age;
+        return retVal;
+      });
     });
+  });
 };
 
 const getData = async () => {
-    // use the async/await pattern to refactor the code above and make the tests pass
-    // you should return an object in the same shape as the `retVal` above
+  // use the async/await pattern to refactor the code above and make the tests pass
+  // you should return an object in the same shape as the `retVal` above
 };
 
 const handleMultiplePromises = async (promises = []) => {
-    // use promise.all to resolve multiple promises
+  // use promise.all to resolve multiple promises
 };
 
+/**
+ * Implement Promise.all
+ * @param {Array} promises an array of promises
+ * @returns {Array} an array of responses returned from a set of promises
+ */
+const myPromiseAll = async (promises = []) => {};
+
 module.exports = {
-    getDataPromiseChain,
-    getData,
-    handleMultiplePromises,
-    fakeApiCall,
+  getDataPromiseChain,
+  getData,
+  handleMultiplePromises,
+  fakeApiCall,
+  myPromiseAll,
 };
--- src/async/promises.test.js
+++ src/async/promises.test.js
@@ -1,53 +1,110 @@
 const {
-    getData,
-    getDataPromiseChain,
-    handleMultiplePromises,
-    fakeApiCall,
-} = require('./promises');
+  getData,
+  getDataPromiseChain,
+  handleMultiplePromises,
+  fakeApiCall,
+  myPromiseAll,
+} = require("./promises");
 
-describe.skip('getDataPromiseChain', () => {
-    it('returns an object with name, age and job', () => {
-        getDataPromiseChain().then((res) => {
-            expect(res).toStrictEqual({
-                name: 'Heather',
-                job: 'code monkey',
-                age: 420,
-            });
-        });
+describe.skip("getDataPromiseChain", () => {
+  it("returns an object with name, age and job", () => {
+    getDataPromiseChain().then((res) => {
+      expect(res).toStrictEqual({
+        name: "Heather",
+        job: "code monkey",
+        age: 420,
+      });
     });
+  });
 });
 
-describe.skip('getData', () => {
-    it('returns an object with name, age and job', async () => {
-        const data = await getData();
-        expect(data).toStrictEqual({
-            name: 'Heather',
-            job: 'code monkey',
-            age: 420,
-        });
+describe.skip("getData", () => {
+  it("returns an object with name, age and job", async () => {
+    const data = await getData();
+    expect(data).toStrictEqual({
+      name: "Heather",
+      job: "code monkey",
+      age: 420,
     });
+  });
 });
 
-describe.skip('handleMultiplePromises', () => {
-    it('returns an array of objects returned from a set of promises', async () => {
-        const [promise1, promise2, promise3] = [
-            fakeApiCall({ name: 'Jill' }),
-            fakeApiCall({ name: 'Jack' }),
-            fakeApiCall({ name: 'John' }),
-        ];
+describe.skip("handleMultiplePromises", () => {
+  it("returns an array of objects returned from a set of promises", async () => {
+    const [promise1, promise2, promise3] = [
+      fakeApiCall({ name: "Jill" }),
+      fakeApiCall({ name: "Jack" }),
+      fakeApiCall({ name: "John" }),
+    ];
 
-        expect(
-            await handleMultiplePromises([promise1, promise2, promise3])
-        ).toStrictEqual([
-            {
-                data: { name: 'Jill' },
-            },
-            {
-                data: { name: 'Jack' },
-            },
-            {
-                data: { name: 'John' },
-            },
-        ]);
+    expect(
+      await handleMultiplePromises([promise1, promise2, promise3])
+    ).toStrictEqual([
+      {
+        data: { name: "Jill" },
+      },
+      {
+        data: { name: "Jack" },
+      },
+      {
+        data: { name: "John" },
+      },
+    ]);
+  });
+});
+
+describe("implements promise all", () => {
+  it("returns an array of responses returned from a set of promises", async () => {
+    const apiCall1 = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "1" });
+      }, 500);
     });
+
+    const apiCall2 = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "2" });
+      }, 200);
+    });
+
+    const apiCall3 = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "3" });
+      }, 100);
+    });
+
+    expect(await myPromiseAll([apiCall1, apiCall2, apiCall3])).toStrictEqual([
+      { message: "1" },
+      { message: "2" },
+      { message: "3" },
+    ]);
+
+    expect(Promise.all).toBeCalledTimes(0);
+  });
+
+  it("rejects if 1 promise fails", async () => {
+    jest.spyOn("Promise", "all");
+
+    const apiCall1 = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "1" });
+      }, 500);
+    });
+
+    const apiCall2 = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "2" });
+      }, 200);
+    });
+
+    const apiCall3 = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(new Error("BOOM"));
+      }, 100);
+    });
+
+    await myPromiseAll([apiCall1, apiCall2, apiCall3]).catch((e) => {
+      expect(e).toEqual(new Error("BOOM"));
+    });
+  });
 });
